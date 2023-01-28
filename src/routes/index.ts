4dd1fe3e17e6fe304e7e178832aa33dbcface2c0
tristan <tristan.lps@gmail.com>
2023-01-28 20:07:43 +0800

    rm template API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
-import UserRouter from './Users';
 import FeedRouter from './Feeds';
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
-router.use('/user', UserRouter);
 router.use('/feed', FeedRouter);
 
 // Export the base-router
